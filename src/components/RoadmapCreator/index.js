7e1ef1f4f5c84383db8ff6852d3138ccbe8e9916
Flober <andrewchen14250@gmail.com>
2021-03-07 17:34:30 -0500

    set up functional example of nodes rendering

--- src/components/RoadmapCreator/index.js
+++ src/components/RoadmapCreator/index.js
@@ -1,78 +1,141 @@
-import React, { useState, useRef, useEffect, useReducer, useContext } from "react"
-import * as d3 from "d3"
-import { nodes as nodesData, links as linksData } from "./data.js"
+import React, {
+  useState,
+  useRef,
+  useEffect,
+  useReducer,
+  useContext,
+} from "react";
+import * as d3 from "d3";
+import { nodes as nodesData, links as linksData } from "./data.js";
 
 const initialNodes = nodesData;
-const initialLinks = linksData
+const initialLinks = linksData;
 
-export const ViewDataContext = React.createContext()
+export const ViewDataContext = React.createContext();
 const NodesReducer = (state, action) => {
-    switch (action) {
-        case 'addNode':
-            console.log("node added")
-            return state;
-        default:
-            console.log("default case")
-            return state
-    }
-}
+  switch (action) {
+    case "addNode":
+      console.log("node added");
+      return state;
+    default:
+      console.log("default case");
+      return state;
+  }
+};
 
 const LinksReducer = (state, action) => {
-    switch (action) {
-        case 'addLink':
-            console.log("link added")
-            return state;
-        default:
-            console.log("default case")
-            return state
-    }
-}
+  switch (action) {
+    case "addLink":
+      console.log("link added");
+      return state;
+    default:
+      console.log("default case");
+      return state;
+  }
+};
 const RoadmapCreatorView = () => {
-    const viewDataContext = useContext(ViewDataContext)
-    const { nodes, links, nodesDispatch, linksDispatch } = viewDataContext
-    const viewContainer = useRef(null)
-    const width = 2048;
-    const height = 1024;
-    useEffect(() => {
-        d3.select(viewContainer.current)
-            .style("width", width / 2 + "px")
-            .style("height", height / 2 + "px")
-            .style("background", "#ffcc3b26")
-            .on("click", () => { nodesDispatch("addNode") })
-    }, [])
+  const viewDataContext = useContext(ViewDataContext);
+  const { nodes, links, nodesDispatch, linksDispatch } = viewDataContext;
+  const viewContainer = useRef(null);
+  const width = 2048;
+  const height = 1024;
+  useEffect(() => {
+    d3.select(viewContainer.current)
+      .style("width", width / 2 + "px")
+      .style("height", height / 2 + "px")
+      .style("background", "#ffcc3b26")
+      .on("click", () => {
+        nodesDispatch("addNode");
+      });
+  }, []);
 
-    useEffect(() => {
-        d3.select(viewContainer.current)
-            .call((viewContainer) => updateView(viewContainer, { nodes, links }))
-    }, [nodes, links])
-    return <div ref={viewContainer} />
-}
+  useEffect(() => {
+    d3.select(viewContainer.current).call((viewContainer) =>
+      updateView(viewContainer, { nodes, links })
+    );
+    console.log("Nodes updated in creator view");
+  }, [nodes, links]);
+  return <div ref={viewContainer} />;
+};
 
 const updateView = (viewContainer, { nodes, links }) => {
-    
-    console.log({viewContainer})
-    const circle1 = (viewContainer).append("svg").attr("viewBox", "-15 15 15 15").style("position", "absolute").attr("transform", "translate(100)").append("circle").attr("r", 20)
-    const circle2 =(viewContainer).append("svg").style("position", "absolute").attr("transform", "translate(75)").append("circle").attr("r", 30)
-    const editable = viewContainer.append("div").style("position", "absolute").attr("contenteditable", true).text("akdlfj")
-}
+  console.log({ viewContainer });
+  console.log({ nodes }, { links });
+
+  /*
+    <div class="everything-container">
+        <svg><circle/></svg>
+        <div contentEditable></div>
+        <svg><circle/></svg>
+        <div contentEditable></div>
+    </div>
+
+
+  */
+  const circleNodesData = nodes.filter((node) => node.type === "circle");
+  const textNodesData = nodes.filter((node) => node.type === "text");
+
+  console.log({ circleNodesData });
+
+  const nodesEnter = viewContainer
+    .selectAll(".circleNodeContainer,.textNodeContainer")
+    .data(nodes)
+    .enter();
+
+  const circleNodesEnter = viewContainer
+    .selectAll(".circleNodeContainer")
+    .data(circleNodesData)
+    .enter();
+  const textNodesEnter = viewContainer
+    .selectAll(".textNodeContainer")
+    .data(textNodesData)
+    .enter();
+
+  circleNodesEnter
+    .append("svg")
+    .style("position", "absolute")
+    .attr("width", "100%")
+    .attr("height", "100%")
+    .attr("class", "circleNodeContainer")
+    .append("circle")
+    .attr("r", 30)
+    .attr("cx", (d) => d.x)
+    .attr("cy", (d) => d.y);
+
+  textNodesEnter
+    .append("div")
+    .style("position", "absolute")
+    .style("transform", (d) => `translate(${d.x}px, ${d.y}px)`)
+    .attr("contenteditable", true)
+    .text("akdlfj");
+};
 
 const Toolbar = () => {
-    const viewDataContext = useContext(ViewDataContext)
-    const { nodes, links, nodesDispatch, linksDispatch } = viewDataContext
+  const viewDataContext = useContext(ViewDataContext);
+  const { nodes, links, nodesDispatch, linksDispatch } = viewDataContext;
 
-    return <div onClick={() => nodesDispatch("addNode")}>add a god damn node</div>
-}
+  return (
+    <div onClick={() => nodesDispatch("addNode")}>add a god damn node</div>
+  );
+};
 
 const RoadmapCreator = () => {
+  const [nodes, nodesDispatch] = useReducer(NodesReducer, initialNodes);
+  const [links, linksDispatch] = useReducer(LinksReducer, initialLinks);
 
-    const [nodes, nodesDispatch] = useReducer(NodesReducer, initialNodes)
-    const [links, linksDispatch] = useReducer(LinksReducer, initialLinks)
-
-    return <ViewDataContext.Provider value={{ nodes: nodes, nodesDispatch: nodesDispatch, links: links, linksDispatch: linksDispatch }}>
-        <Toolbar />
-        <RoadmapCreatorView />
+  return (
+    <ViewDataContext.Provider
+      value={{
+        nodes: nodes,
+        nodesDispatch: nodesDispatch,
+        links: links,
+        linksDispatch: linksDispatch,
+      }}
+    >
+      <Toolbar />
+      <RoadmapCreatorView />
     </ViewDataContext.Provider>
+  );
+};
 
-}
-
-export default RoadmapCreator; 
+export default RoadmapCreator;

950623148feedb0fa37880ae76a262bdd7e3a7f2
Flober <andrewchen14250@gmail.com>
2021-03-07 15:59:23 -0500

    viewDataContext setup

--- src/components/RoadmapCreator/index.js
+++ src/components/RoadmapCreator/index.js
@@ -29,43 +29,48 @@ const LinksReducer = (state, action) => {
 }
 const RoadmapCreatorView = () => {
     const viewDataContext = useContext(ViewDataContext)
-    const {nodes, links, nodesDispatch, linksDispatch} = viewDataContext
+    const { nodes, links, nodesDispatch, linksDispatch } = viewDataContext
     const viewContainer = useRef(null)
     const width = 2048;
     const height = 1024;
     useEffect(() => {
         d3.select(viewContainer.current)
-            .attr('viewBox', [-width / 2, -height / 2, width, height])
-            .style("background", "blue")
+            .style("width", width / 2 + "px")
+            .style("height", height / 2 + "px")
+            .style("background", "#ffcc3b26")
             .on("click", () => { nodesDispatch("addNode") })
     }, [])
 
     useEffect(() => {
         d3.select(viewContainer.current)
-            .call((svg) => updateView(svg, { nodes, links }))
+            .call((viewContainer) => updateView(viewContainer, { nodes, links }))
     }, [nodes, links])
-    return <svg ref={viewContainer} />
+    return <div ref={viewContainer} />
 }
 
-const updateView = (svg, { nodes, links }) => {
-
+const updateView = (viewContainer, { nodes, links }) => {
+    
+    console.log({viewContainer})
+    const circle1 = (viewContainer).append("svg").attr("viewBox", "-15 15 15 15").style("position", "absolute").attr("transform", "translate(100)").append("circle").attr("r", 20)
+    const circle2 =(viewContainer).append("svg").style("position", "absolute").attr("transform", "translate(75)").append("circle").attr("r", 30)
+    const editable = viewContainer.append("div").style("position", "absolute").attr("contenteditable", true).text("akdlfj")
 }
 
 const Toolbar = () => {
     const viewDataContext = useContext(ViewDataContext)
-    const {nodes, links, nodesDispatch, linksDispatch} = viewDataContext
+    const { nodes, links, nodesDispatch, linksDispatch } = viewDataContext
 
-return <div onClick={() => nodesDispatch("addNode")}>add a god damn node</div>
+    return <div onClick={() => nodesDispatch("addNode")}>add a god damn node</div>
 }
 
 const RoadmapCreator = () => {
-    
+
     const [nodes, nodesDispatch] = useReducer(NodesReducer, initialNodes)
     const [links, linksDispatch] = useReducer(LinksReducer, initialLinks)
 
     return <ViewDataContext.Provider value={{ nodes: nodes, nodesDispatch: nodesDispatch, links: links, linksDispatch: linksDispatch }}>
-            <Toolbar/>
-            <RoadmapCreatorView />
+        <Toolbar />
+        <RoadmapCreatorView />
     </ViewDataContext.Provider>
 
 }

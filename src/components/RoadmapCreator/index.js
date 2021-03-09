773ba8a3a413dbc18fb0a4c17d9edf47da76a9c8
Flober <andrewchen14250@gmail.com>
2021-03-09 10:30:50 -0500

    text, circle nodes render sequentially to solve raise() issue

--- src/components/RoadmapCreator/index.js
+++ src/components/RoadmapCreator/index.js
@@ -98,38 +98,21 @@ const RoadmapCreatorView = () => {
   useEffect(() => {
     console.log("nodes updated, new nodes: ", { nodes });
     d3.select(viewContainer.current).call((viewContainer) => {
-      updateView(
-        viewContainer,
-        d3.select(circleNodesContainer.current),
-        d3.select(textNodesContainer.current),
-        {
-          nodes,
-          links,
-        }
-      );
+      updateView(viewContainer, {
+        nodes,
+        links,
+      });
     });
   }, [nodes, links]);
   return (
     <>
       <Toolbar setMode={(mode) => setMode(mode)} mode={mode} />
-      <div ref={viewContainer}>
-        <svg
-          ref={circleNodesContainer}
-          style={{ width: width, height: height, position: "absolute" }}
-        ></svg>
-        <div ref={textNodesContainer}></div>
-      </div>
-      ;
+      <div ref={viewContainer}></div>;
     </>
   );
 };
 
-const updateView = (
-  viewContainer,
-  circleNodesContainer,
-  textNodesContainer,
-  { nodes, links }
-) => {
+const updateView = (viewContainer, { nodes, links }) => {
   /* structure: 
     <div class="everything-container">
         <svg><circle/></svg>
@@ -138,50 +121,55 @@ const updateView = (
         <div contentEditable></div>
     </div>
   */
-
   const circleNodesData = nodes.filter((node) => node.type === "circle");
   const textNodesData = nodes.filter((node) => node.type === "text");
+
+  const nodesEnter = viewContainer
+    .selectAll(`.${circleElementName},.${textElementName}`)
+    .data(nodes)
+    .enter();
+
   const textElementName = "textNodeContainer";
-  const circleElementName = "circleNode";
-  const circleNodesEnter = circleNodesContainer
-    .selectAll(`.${circleElementName}`)
+  const circleElementName = "circleNodeContainer";
+  const circleStrokeWidth = 3;
+  const radius = 30;
+  nodesEnter.each((d, i, list) => {
+    if (d.type === "circle") {
+      viewContainer.append("svg").attr("class", circleElementName);
+    } else if (d.type === "text") {
+      viewContainer.append("div").attr("class", textElementName);
+    }
+  });
+  var circleNodes = viewContainer.selectAll(`.${circleElementName}`);
+  var textNodes = viewContainer.selectAll(`.${textElementName}`);
+  circleNodes
     .data(circleNodesData)
-    .enter();
-
-  const textNodesEnter = textNodesContainer
-    .selectAll(`.${textElementName}`)
-    .data(textNodesData)
-    .enter();
-
-  circleNodesEnter
+    .style("position", "absolute")
+    .attr("width", 2 * (radius + circleStrokeWidth))
+    .attr("height", 2 * (radius + circleStrokeWidth))
+    .style("transform", (d) => `translate(${d.x}px, ${d.y}px)`)
+    .call(attachCircleDrag())
     .append("circle")
-    .attr("class", circleElementName)
-    .attr("r", 30)
-    .attr("cx", (d) => d.x)
-    .attr("cy", (d) => d.y)
+    .attr("r", radius)
+    .attr("cx", radius + circleStrokeWidth)
+    .attr("cy", radius + circleStrokeWidth)
     .style("fill", "#FFFFFF")
     .style("stroke", "#000000")
-    .style("stroke-width", "3px")
-    .call(attachCircleDrag());
+    .style("stroke-width", `${circleStrokeWidth}px`);
 
-  var textNodesContainer = textNodesEnter
-    .append("div")
-    .attr("class", textElementName)
+  textNodes
+    .data(textNodesData)
     .style("padding", "10px 20px 10px 20px")
     .style("position", "absolute")
     .style("transform", (d) => `translate(${d.x}px, ${d.y}px)`)
     .style("border", "1px solid black")
     .style("background", "#FFFFFF")
     .style("border-radius", "7px")
-    .call(attachTextDrag());
-
-  textNodesContainer
+    .call(attachTextDrag())
     .append("div")
     .attr("contenteditable", true)
-    .text("akdlfj")
-    .call(() => {
-      return d3.drag().on("start", null).on("drag", null).on("end", null);
-    });
+    .text("akdlfj");
+
   function attachCircleDrag() {
     return d3
       .drag()
@@ -190,19 +178,20 @@ const updateView = (
       .on("end", dragended);
 
     function dragstarted(d) {
-      circleNodesContainer.raise();
+      //circleNodesContainer.raise();
       d3.select(this).raise().classed("active", true);
     }
 
     function dragged(d) {
-      d3.select(this)
-        .attr("cx", (d.x = d3.event.x))
-        .attr("cy", (d.y = d3.event.y));
+      d3.select(this).style(
+        "transform",
+        `translate(${(d.x = d3.event.x)}px, ${(d.y = d3.event.y)}px)`
+      );
     }
 
     function dragended(d) {
       d3.select(this).classed("active", false);
-      circleNodesContainer.lower();
+      //circleNodesContainer.lower();
     }
   }
 
